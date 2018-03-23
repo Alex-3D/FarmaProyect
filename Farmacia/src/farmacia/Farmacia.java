33f47a8cb2c50fe10ad14c17fddd6ad19e98974d
jan7mil <jannet7mil@gmail.com>
2018-03-22 22:00:04 -0300

    Se suben las primeras clases y estructura del modelo en Java
    Se suben las primeras clases y estructura del modelo en Java

---
+++ Farmacia/src/farmacia/Farmacia.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package farmacia;
+
+/**
+ *
+ * @author Alex3D
+ */
+public class Farmacia {
+
+    private Inventario inventario;
+    //proveedores (??
+    private Caja laCaja;
+    //Controlador
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
